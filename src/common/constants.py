b1a82088a0084d4b7041ce509c14166d23e87492
Alexandrino Santana de Souza <alexandrino.souza@memed.com.br>
2023-04-30 00:07:48 -0300

    Change btc qty

--- src/common/constants.py
+++ src/common/constants.py
@@ -2,6 +2,6 @@ SYMBOL = 'BTCBRL'
 START_TIME = '6 hours ago UTC'  # to start for 1 week ago
 INTERVAL = '1m'
 LIMIT_open_order_PER_DAY = 5
-BTC_QTD = '0.00008'
+BTC_QTD = '0.00007'
 PROFIT_PERCENT = 0.003
 LOSS_PERCENT = 0.008
